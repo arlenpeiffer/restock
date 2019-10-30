7c1b93bc50e469a0bb7fed3a3a18e25687e1ac1b
Arlen Peiffer <arlenpeiffer@gmail.com>
2019-10-30 12:13:00 -0700

    WIP setting up Checklist screen

--- src/components/screens/Checklist.js
+++ src/components/screens/Checklist.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Text, Button } from 'react-native';
+
+import OrderContext from 'contexts/OrderContext';
 import ScreenContainer from 'common/ScreenContainer';
 
 const Checklist = props => {
+  const { order } = useContext(OrderContext);
+
+  const createOrderArray = () => {
+    let orderArray = [];
+
+    Object.entries(order).map(([key, value]) => {
+      orderArray.push({
+        name: key,
+        amount: value,
+        isChecked: false
+      });
+    });
+
+    return orderArray;
+  };
+
   return (
     <ScreenContainer>
       <Text>Checklist Screen</Text>
+      {createOrderArray().map(item => (
+        <Text key={item.name}>
+          {item.amount} {item.name}
+        </Text>
+      ))}
       <Button
         title="Go to Form Screen"
         onPress={() => props.navigation.navigate('Form')}
