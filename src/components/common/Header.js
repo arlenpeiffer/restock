d95f2d06c1ed574ec423acef31311f78a003fde3
Arlen Peiffer <arlenpeiffer@gmail.com>
2019-11-05 13:45:52 -0800

    WIP refactoring appearance of Header

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -2,35 +2,35 @@ import React from 'react';
 import styled from 'styled-components/native';
 
 import { colors } from 'constants/colors';
-import logo from 'images/la-colombe-coffee-roasters-logo-vector.png';
+import headerLogo from 'images/logo-la-colombe.png';
 
 const HeaderContainer = styled.View`
   align-items: center;
-  background-color: white;
+  background-color: ${colors.EGYPTIAN_BLUE};
   justify-content: center;
-  padding-bottom: 15px;
 `;
 
 const Logo = styled.Image`
-  height: 100;
+  height: 50px;
+  margin-top: 30px;
   resize-mode: contain;
 `;
 
 const NavigationContainer = styled.View`
   flex-direction: row;
-  margin-top: -15px;
+  margin: 20px 0;
 `;
 
 const NavigationItem = styled.TouchableOpacity`
   border-bottom-color: ${props =>
-    props.isSelected ? `${colors.SAFFRON}` : 'white'};
+    props.isSelected ? `${colors.SAFFRON}` : `${colors.EGYPTIAN_BLUE}`};
   border-bottom-width: 5px;
   margin: 0 25px;
   padding-bottom: 5px;
 `;
 
 const NavigationItemLabel = styled.Text`
-  color: ${colors.MIDNIGHT_BLUE};
+  color: white;
   font-size: 16;
   font-weight: 700;
   letter-spacing: 1.5;
@@ -40,7 +40,7 @@ const NavigationItemLabel = styled.Text`
 const Header = props => {
   return (
     <HeaderContainer>
-      <Logo source={logo} />
+      <Logo source={headerLogo} />
       <NavigationContainer>
         <NavigationItem
           activeOpacity={1}
