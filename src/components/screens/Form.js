e9855d1a34e7fc9cd7eada3927d84f54bc096f12
Arlen Peiffer <arlenpeiffer@gmail.com>
2019-11-05 21:48:52 -0800

    Refactors ScreenContainer and wrapper components in Checklist and Form

--- src/components/screens/Form.js
+++ src/components/screens/Form.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import { SafeAreaView } from 'react-native';
+import { SafeAreaView, ScrollView } from 'react-native';
 
 import ScreenContainer from 'common/ScreenContainer';
-import Section from 'form/Section';
 import { inventory } from 'data/inventory';
+import Section from 'form/Section';
 
 const Form = () => {
   return (
     <SafeAreaView>
-      <ScreenContainer>
-        {inventory.map(section => (
-          <Section key={section.name} section={section} />
-        ))}
-      </ScreenContainer>
+      <ScrollView>
+        <ScreenContainer>
+          {inventory.map(section => (
+            <Section key={section.name} section={section} />
+          ))}
+        </ScreenContainer>
+      </ScrollView>
     </SafeAreaView>
   );
 };
