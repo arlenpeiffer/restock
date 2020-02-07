47715978762d39a0609ac80de8613a951b9c4122
Arlen Peiffer <arlenpeiffer@gmail.com>
2020-02-07 15:51:19 -0800

    makes changes to Checklist screen

--- src/components/screens/Checklist.js
+++ src/components/screens/Checklist.js
@@ -1,27 +1,43 @@
 import React, { useContext } from 'react';
-import { SafeAreaView, ScrollView, Text } from 'react-native';
+import { SafeAreaView, ScrollView } from 'react-native';
+import styled from 'styled-components/native';
 
 import Section from 'checklist/Section';
 import ScreenContainer from 'common/ScreenContainer';
 import OrderContext from 'contexts/OrderContext';
 
+const EmptyPrompt = styled.View`
+  flex: 1;
+  justify-content: center;
+  margin-horizontal: 15%;
+`;
+
+const Text = styled.Text`
+  font-size: 18;
+  text-align: center;
+`;
+
 const Checklist = () => {
   const { order } = useContext(OrderContext);
+  const orderContainsItems = order.length > 0;
 
-  return (
+  return orderContainsItems ? (
     <SafeAreaView>
       <ScrollView>
         <ScreenContainer>
-          {order.length > 0 ? (
-            order.map(section => (
-              <Section key={section.name} section={section} />
-            ))
-          ) : (
-            <Text>You're all stocked up!</Text>
-          )}
+          {order.map(section => (
+            <Section key={section.name} section={section} />
+          ))}
         </ScreenContainer>
       </ScrollView>
     </SafeAreaView>
+  ) : (
+    <EmptyPrompt>
+      <Text>
+        Add a few items to your Checklist using the Order Form. They'll show up
+        here ☕
+      </Text>
+    </EmptyPrompt>
   );
 };
 
